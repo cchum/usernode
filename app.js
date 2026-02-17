44b3da1d77ae136136cb2ac87701a100303ceaa9
cchum <77558145+cchum@users.noreply.github.com>
2026-02-16 19:20:34 -0700

    connectin to the data base

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const url = 'mongodb://localhost/UserDB'
+
+const app = express()
+
+
+mongoose.connect(url)
+const con = mongoose.connection
+
+con.on('open', function(){
+    console.log('connected successfully...')
+})
+
+const userRouter = require('./routes/users')
+app.use('/users',userRouter)
+
+app.listen(9000, () => {
+    console.log('Server has started')
+})
