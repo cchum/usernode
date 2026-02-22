90fac21410e951c08b027d83425f757a248aeb29
cchum <77558145+cchum@users.noreply.github.com>
2026-02-22 07:59:43 -0700

    two new schema files

--- routes/models/conversation.js
+++ routes/models/conversation.js
@@ -1,1 +1,12 @@
 const mongoose = require('mongoose')
+
+const conversationSchema = new mongoose.Schema({
+    // "userA" : "id", not implemnted yet
+    // "userB": "id", not implemented yet
+    // "covnersation": "string" nnot implemented yet
+})
+
+
+
+
+module.exports = mongoose.model('Conversation', conversationSchema)
--- routes/models/message.js
+++ routes/models/message.js
@@ -1,1 +1,12 @@
 const mongoose = require('mongoose')
+
+const messageSchema = new mongooseSchema({
+    // "senderID": id, still thinking how to implement
+    // "receiverID": id, still thinking how to implement
+    text: {
+        type: String,
+        require: true
+    }
+})
+
+module.exports = mongoose.model('Message', messageSchema)
