350b722198c6a5e739919b0a99a368e4c4bdcb41
cchum <77558145+cchum@users.noreply.github.com>
2026-02-22 19:01:35 -0700

    post working now

--- routes/messages.js
+++ routes/messages.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const router = express.Router()
 const Message = require('./models/message')
 const Conversation = require('./models/conversation')
@@ -40,6 +41,27 @@ router.post('/', async(req, res) => {
         res.status(201).json(message)
     } catch (err) {
         res.status(500).json(({ error: err.message }))
+    }
+})
+
+router.patch('/:messageId', async(req, res) => {
+    const { messageId } = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(messageId)){
+        return res.status(400).json({ error: 'Invalid message ID'})
+    }
+
+    try {
+        const message = await Message.findById(messageId)
+        if (!message) {
+            return res.status(404).json({ error: "Message not found" })
+        }
+
+        message.text = req.body.text
+        const updatedMessage = await message.save()
+        res.status(200).json(updatedMessage)
+    } catch(err) {
+        res.status(500).json(({error: err.message}))
     }
 })
 
